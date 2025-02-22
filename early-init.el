e38e6bf6ac25892c854868d1f9de378f7ea41d79
Griffen Wakelin <wakelin@dal.ca>
2025-02-21 23:42:48 -0400

    added missing files

--- early-init.el
+++ early-init.el
@@ -1,12 +1,19 @@
-;; -*- lexical-binding: t -*-
-
+;;; early-init.el --- Early initialization. -*- lexical-binding: t -*-
+;;
+;;; Commentary:
+;;
 ;; This file is loaded before package.el is initialized, and
 ;; before the first graphical frame is initialized in Emacs 27+.
+;;
+;;; Code:
 
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+(setq auto-mode-case-fold nil)
+(setq-default bidi-display-reordering 'left-to-right
+              bidi-paragraph-direction 'left-to-right)
+(setq bidi-inhibit-bpa t)
+(setq idle-update-delay 1.0)
 
+;; defer gc
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.5)
 
@@ -15,14 +22,35 @@
             (setq gc-cons-threshold (* 1000 1000 8)
                   gc-cons-percentage 0.1)))
 
+;; prevent loading any packages prior to init.el
 (setq package-enable-at-startup t)
+
+;; increase scrolling performance
+(setq redisplay-skip-fontification-on-input t)
+(setq fast-but-imprecise-scrolling t)
+
+;; faster to disable these before they have been initialized
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+;; start scratch buffer in `fundamental-mode' instead (performance boost)
+(setq initial-major-mode 'fundamental-mode)
+
+;; ensure eln-cache is set
+(unless (version-list-<
+         (version-to-list emacs-version)
+         '(28 0 1 0))
+  (when (boundp 'native-comp-eln-load-path)
+  (add-to-list 'native-comp-eln-load-path
+               (expand-file-name "eln-cache/" user-emacs-directory))))
 
 (setq default-frame-alist
       (append default-frame-alist
               '((left   . 200)
                 (top    . 200)
-                (width  . 130)
-                (height . 31))))
+                (width  . 145)
+                (height . 30))))
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 
-(add-hook 'after-init-hook (lambda () (set-frame-name "home")))
+;;; early-init.el ends here
