fc8a600ab16737cb4d3a2a218d77956f60c7a001
Griffen Wakelin <wakelin@dal.ca>
2025-02-23 13:00:35 -0400

    changed global keymaps to use `define-key global-map' instead of `global-set-key'

--- config/keybinds.el
+++ config/keybinds.el
@@ -6,15 +6,20 @@
   :config
   (which-key-mode))
 
-(global-set-key (kbd "C-x k") 'gw/kill-this-buffer)
-(global-set-key (kbd "C-c rr") 'gw/org-roam-rg-search)
-(global-set-key (kbd "C-c ri") 'org-roam-node-insert)
-(global-set-key (kbd "C-x C-r") 'counsel-recentf)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
+;; global mappings
+(define-key global-map (kbd "C-x k") 'gw/kill-this-buffer)
+(define-key global-map (kbd "C-c rr") 'gw/org-roam-rg-search)
+(define-key global-map (kbd "C-c ri") 'org-roam-node-insert)
+(define-key global-map (kbd "C-x C-r") 'counsel-recentf)
+(define-key global-map (kbd "C-c a") 'org-agenda)
+(define-key global-map (kbd "C-c c") 'org-capture)
+(define-key global-map (kbd "C-c z") 'reveal-in-osx-finder)
+(define-key global-map (kbd "C-g") #'gw/keyboard-quit-dwim)
 
-(global-set-key (kbd "C-c z") 'reveal-in-osx-finder)
-
+;; mode-specific mappings
 (define-key calendar-mode-map "]" #'org-reverse-datetree-calendar-next)
 (define-key calendar-mode-map "[" #'org-reverse-datetree-calendar-previous)
 (define-key calendar-mode-map (kbd "RET") #'org-reverse-datetree-display-entry)
+(define-key evil-normal-state-map (kbd "L") 'tab-next)
+(define-key evil-normal-state-map (kbd "H") 'tab-previous)
+
