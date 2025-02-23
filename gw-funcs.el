5d5203a5efce34c3ad2fa5b631d35e47fa2a3fe2
Griffen Wakelin <wakelin@dal.ca>
2025-02-23 12:49:56 -0400

    added commands to restart emacs-server & generalize <C-g> for use on more things. both functions do not seem to work

--- gw-funcs.el
+++ gw-funcs.el
@@ -196,3 +196,35 @@ this can take a second or two to execute."
     (if $result
         (string-trim $result)
       "")))
+
+(defun gw/restart-server ()
+  "Restart the Emacs server."
+  (interactive)
+  (server-force-delete)
+  (while (server-running-p)
+    (sleep-for 1))
+  (server-start))
+
+(defun gw/keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open. Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case, use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
