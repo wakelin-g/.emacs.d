dce093bb27a09e0bffe9e0f1910063a3cd7dd279
Griffen Wakelin <wakelin@dal.ca>
2025-02-23 13:01:06 -0400

    switched from doom-modeline to smart-mode-line

--- config/modeline.el
+++ config/modeline.el
@@ -1,19 +1,26 @@
 ;;; -*- lexical-binding: t -*-
 
-(use-package doom-modeline
+;; (use-package doom-modeline
+;;   :straight t
+;;   :ensure t
+;;   :init (doom-modeline-mode 1)
+;;   :config
+;;   (setq doom-modeline-support-imenu t
+;;         doom-modeline-height 25
+;;         doom-modeline-bar-width 0.1
+;;         doom-modeline-hud t
+;;         doom-modeline-buffer-file-name-style 'truncate-nil
+;;         doom-modeline-icon t
+;;         doom-modeline-major-mode-icon t
+;;         doom-modeline-major-mode-color-icon t
+;;         doom-modeline-buffer-state-icon t
+;;         doom-modeline-buffer-modification-icon t
+;;         doom-modeline-time-icon t
+;;         doom-modeline-buffer-name t))
+
+(setq sml/no-confirm-load-theme t)
+(use-package smart-mode-line
   :straight t
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :config
-  (setq doom-modeline-support-imenu t
-        doom-modeline-height 25
-        doom-modeline-bar-width 0.1
-        doom-modeline-hud t
-        doom-modeline-buffer-file-name-style 'truncate-nil
-        doom-modeline-icon t
-        doom-modeline-major-mode-icon t
-        doom-modeline-major-mode-color-icon t
-        doom-modeline-buffer-state-icon t
-        doom-modeline-buffer-modification-icon t
-        doom-modeline-time-icon t
-        doom-modeline-buffer-name t))
+  :init
+  (setq sml/theme 'respectful)
+  (sml/setup))
