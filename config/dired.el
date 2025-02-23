e761bcf78bca522adf764ccecda01d70d714e3ff
Griffen Wakelin <wakelin@dal.ca>
2025-02-23 12:59:24 -0400

    changed dirvish opts

--- config/dired.el
+++ config/dired.el
@@ -7,28 +7,12 @@
   (setq dired-listing-switches "-ahlF"))
 
 (use-package dirvish
-  :straight t
+  :straight (dirvish :type git :files (:defaults "extensions/*.el" "dirvish-pkg.el") :host github :repo "alexluigit/dirvish")
   :init
   (dirvish-override-dired-mode)
-  :custom
-  (dirvish-quick-access-entries
-   '(("h" "~/"            "Home")
-     ("d" "~/Downloads"   "Downloads")
-     ("m" "/Volumes"      "Volumes")
-     ("o" "~/orgmode"     "Org")))
   :config
   (dirvish-peek-mode)
   (dirvish-side-follow-mode)
-  (setq dirvish-mode-line-format '(:left (sort symlink) :right (omit yank index)))
-  (setq dirvish-mode-line-height 10)
-  (setq dirvish-attributes '(nerd-icons file-time file-size collapse subtree-state vc-state git-msg))
-  (setq dirvish-subtree-state-style 'nerd)
-  (setq delete-by-moving-to-trash t)
-  (setq dirvish-path-separators (list
-                                 (format "  %s " (nerd-icons-codicon "nf-cod-home"))
-                                 (format "  %s " (nerd-icons-codicon "nf-cod-root_folder"))
-                                 (format " %s " (nerd-icons-faicon "nf-fa-angle_right"))))
-  (setq dirvish-listing-switches "-l --almost-all --human-readable --group-directories-first --no-groups")
   :bind
   (("C-c f" . dirvish)
    :map dirvish-mode-map
@@ -49,3 +33,27 @@
    ("M-s" . dirvish-setup-menu)
    ("M-e" . dirvish-emerge-menu)
    ("M-j" . dirvish-fd-jump)))
+
+(with-eval-after-load 'dirvish
+  (add-hook 'dirvish-setup-hook 'dirvish-emerge-mode)
+  (setq dirvish-mode-line-format '(:left (sort symlink) :right (vc-info yank index)))
+  (setq dirvish-mode-line-height 21)
+  (setq dirvish-header-line-height '(25 . 35))
+  (setq dirvish-header-line-format '(:left (path) :right (free-space)))
+  (setq dirvish-path-separators (list "  " "  " "  "))
+  (setq dirvish-side-width 38)
+  (setq dirvish-attributes '(nerd-icons file-time file-size collapse subtree-state vc-state git-msg))
+  (setq dirvish-subtree-state-style 'nerd)
+  (setq delete-by-moving-to-trash t)
+  (setq dirvish-path-separators (list
+                                 (format "  %s " (nerd-icons-codicon "nf-cod-home"))
+                                 (format "  %s " (nerd-icons-codicon "nf-cod-root_folder"))
+                                 (format " %s " (nerd-icons-faicon "nf-fa-angle_right"))))
+  (setq dirvish-listing-switches "-l --almost-all --human-readable --group-directories-first --no-groups"))
+
+(with-eval-after-load 'dirvish-quick-access
+  (setopt dirvish-quick-access-entries
+   '(("h" "~/"            "Home")
+     ("d" "~/Downloads"   "Downloads")
+     ("m" "/Volumes"      "Volumes")
+     ("o" "~/orgmode"     "Org"))))
